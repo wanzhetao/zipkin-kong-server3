e4f7f6ceb7cc1530993678ec398ed73ebb22c4b5
wzt <wzt@LAPTOP-5LOO6GCT>
2020-05-01 19:47:56 +0800

    zipkin-kong-server3

---
+++ src/main/java/com/vispractice/zipkin/service3/ZipkinBraveController.java
@@ -0,0 +1,33 @@
+package com.vispractice.zipkin.service3;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vispractice.zipkin.TracelinkCutdown;
+import com.vispractice.zipkin.UtilId;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api("service的API接口")
+@RestController
+@RequestMapping("/service3")
+public class ZipkinBraveController {
+
+	//单次服务调用的唯一TraceID
+    private String linkTraceID=UtilId.randomHexString(16);
+
+    @ApiOperation("trace第三步")
+    @RequestMapping("/test")
+    public String service1(HttpServletRequest req,HttpServletResponse res) throws Exception {
+
+    	//提供服务改TraceID和SpanID
+        TracelinkCutdown.CutdownKongtoService(req, res, linkTraceID);
+
+        return "service3";
+    }
+
+}
